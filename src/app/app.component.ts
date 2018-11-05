028dbf82c96218734d596b3a9f96b9f3806b84b1
mac <mac@Jade-MBP.local>
2018-11-05 10:14:36 +0700

    add virtual-scrolling and drag-drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { moveItemInArray } from '@angular/cdk/drag-drop'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng7-drapdrop-virtualscrolling';
+  Items = [];
+
+  DropList = [ 'First', 'Second', 'Third', 'Fourth'];
+
+  constructor(){
+    this.initializeList();
+  }
+
+  initializeList(){
+    this.Items = [];
+    for (let index = 0; index < 10000; index++) {
+      this.Items.push(index);
+    }
+  }
+
+  onDrop(event){
+    console.log(event);
+    moveItemInArray(this.DropList, event.previousIndex, event.currentIndex);
+  }
+
+  
 }
